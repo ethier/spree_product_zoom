44e2748b91a6087843be303efd477209cffb3e58
Derek Ethier <derek.ethier@gmail.com>
2013-09-07 06:55:07 -0700

    Update fancybox to 2.1.5

--- vendor/assets/javascripts/fancybox.js
+++ vendor/assets/javascripts/fancybox.js
@@ -2,4 +2,4 @@
 //= require jquery.fancybox-buttons
 //= require jquery.fancybox-thumbs
 //= require jquery.fancybox-media
-//= require jquery.mousewheel-3.0.6.pack
+//= require jquery.mousewheel.pack
